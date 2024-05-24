143daaa49f70c41e8484632c7b7e2d6a84f4837b
Márcio Martins <masterplays02@gmail.com>
2024-05-24 19:05:40 -0300

    Suporte a conversão de documentos

--- Utils/Converter.py
+++ Utils/Converter.py
@@ -1,14 +1,22 @@
 from moviepy.editor import VideoFileClip, AudioFileClip
 from Utils.ErrorDialog import ErrorDialog
-from PIL import Image
-import os
 from pydub import AudioSegment
+from docx import Document
+from fpdf import FPDF
+
+from PIL import Image
+
+from PySide6.QtWidgets import QFileDialog
+
+import os
+
+
 
 class Converter:
     def __init__(self):
         ...
 
-    def convert_image(self, files, convert_to, additional_data=None):
+    def convert_image(self, files, convert_to, additional_data=None, *args):
         print(convert_to)
         for file_path in files:
             try:
@@ -37,7 +45,7 @@ class Converter:
         
         return True
     
-    def convert_video(self, files, convert_to, additional_data=None):
+    def convert_video(self, files, convert_to, additional_data=None, *args):
         codec_mapping = {
             'mp4': 'libx264',
             'avi': 'libxvid',
@@ -87,7 +95,7 @@ class Converter:
         
         return True
 
-    def convert_audio(self, files, convert_to, additional_data=None):
+    def convert_audio(self, files, convert_to, additional_data=None, *args):
         format_mapping = {
             'mp3': 'mp3',
             'wav': 'wav',
@@ -114,3 +122,41 @@ class Converter:
                 return False
 
         return True
+    
+    def convert_document(self, files, convert_to, additional_data=None, output_path=None):
+        try:
+            output_format = convert_to.lower()
+            output_name = additional_data if additional_data else 'output'
+            output_path = output_path if output_path else '.'
+
+            if output_format == 'docx':
+                doc = Document()
+                for file in files:
+                    doc.add_picture(file)
+                output_file_path = os.path.join(output_path, f"{output_name}.docx")
+                doc.save(output_file_path)
+
+            elif output_format == 'pdf':
+                pdf = FPDF()
+                pdf.set_auto_page_break(auto=True, margin=15)  
+
+                for file in files:
+                    image_width, image_height = pdf.get_image_size(file)
+                    page_width = pdf.w - 2 * pdf.l_margin
+                    page_height = pdf.h - 2 * pdf.t_margin
+                    if image_width > page_width or image_height > page_height:
+                        pdf.add_page()
+                    pdf.image(file, x=pdf.l_margin, y=pdf.get_y(), w=page_width)
+                output_file_path = os.path.join(output_path, f"{output_name}.pdf")
+                pdf.output(output_file_path)
+
+            else:
+                ErrorDialog.show_error_dialog( f"Erro ao criar o documento {output_name}.{output_format}: {e}", "Verifique se os arquivos são válidos", "suporte a .DOC está sendo implementado")
+            return False
+
+        except Exception as e:
+            ErrorDialog.show_error_dialog(
+                f"Erro ao criar o documento {output_name}.{output_format}: {e}",
+                "Verifique se os arquivos são válidos", "suporte a .DOC está sendo implementado"
+            )
+            return False
